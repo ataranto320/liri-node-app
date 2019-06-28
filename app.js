6e7b7de78eaba578909092de4a2532947f2ed554
Austin T <austin.taranto@gmail.com>
2019-06-27 22:07:21 -0400

    added spotify id and secret and used example search for ref

---
+++ app.js
@@ -0,0 +1,14 @@
+// examples of npm
+var axios = require("axios")
+var moment = require("moment")
+var dotenv = require("dotenv")
+
+// spotify api
+var spotify = require("spotify");
+
+var spotify = new spotify({
+    id: "1f52bc55b72c4ed48afd607346ab9fd8",
+    secret: "907a3cfcae73421f914ad4bc7dc82c96"
+});
+
+
